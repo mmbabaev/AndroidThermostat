50da3881bc2be7c290ccb7519c6c8c0bcf6ba4d0
yura <yudzhadobin@gmail.com>
2015-05-30 14:08:43 +0300

    commit

---
+++ app/src/main/java/com/example/mihail/hti16/Storage.java
@@ -0,0 +1,12 @@
+package com.example.mihail.hti16;
+
+import com.example.mihail.hti16.Boiler.Boiler;
+import com.example.mihail.hti16.Boiler.DayOfWeek;
+
+import java.util.List;
+
+
+public class Storage {
+    public static List<DayOfWeek> chosenDays;
+    public static Boiler boiler;
+}
